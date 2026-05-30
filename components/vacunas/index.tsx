93ba51713f85bfb6b7a0d398c836a449f331d94c
Hernan <hernanst7@gmail.com>
2026-05-30 09:44:20 -0300

    fix: agregar calculateNextDose al import de vacunas/index.tsx

--- components/vacunas/index.tsx
+++ components/vacunas/index.tsx
@@ -36,6 +36,7 @@ import { clientes, mascotas } from "@/lib/mock-data"
 import {
   buildPetVaccinationHistory,
   buildVaccineDisplayList,
+  calculateNextDose,
   formatInterval,
   formatOverdueText,
   getDoseIntervalPreset,
